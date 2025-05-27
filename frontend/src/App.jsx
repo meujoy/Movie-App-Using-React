5a7bb54e4621a2227e4f32422fdd46646d2bf9c9
meujoy <youssefayman742@gmail.com>
2025-05-27 21:18:41 +0300

    Implemented Movie Card Component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,27 @@
 import './App.css'
+import MovieCard from "./assets/components/MovieCard"
 
 function App() {
+  const movieNumber = 1;
 
   return (
-    <div></div>
+    <>
+      {movieNumber === 1 ?(
+        <MovieCard movie={{title: "Youssef's Film",release_date: "2024"}}></MovieCard>
+      ):(
+        <MovieCard movie={{title: "Joe's Film",release_date: "2023"}}></MovieCard>
+      )}
+    </>
   )
 }
+
+//This a component, it can be used inside of various components
+// function Text({text}) { //{text} is called a prop, it a variable that be passed dinamically to a component 
+//   return (
+//     <div>
+//       <p>{text}</p>
+//     </div>
+//   )
+// }
 
 export default App
