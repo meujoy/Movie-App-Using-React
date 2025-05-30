7b4ff83db8475cfd0cc329af76aa90f68c99cbdb
meujoy <youssefayman742@gmail.com>
2025-05-30 15:40:15 +0300

    implemented the first iteration of the hompage.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,19 @@
 import './App.css'
-import MovieCard from "./assets/components/MovieCard"
+import Home from './pages/home'
+import MovieCard from './assets/components/MovieCard';
 
 function App() {
-  const movieNumber = 1;
-
   return (
     <>
-      {movieNumber === 1 ?(
-        <MovieCard movie={{title: "Youssef's Film",release_date: "2024"}}></MovieCard>
-      ):(
-        <MovieCard movie={{title: "Joe's Film",release_date: "2023"}}></MovieCard>
-      )}
+      <Home>
+        
+      </Home>
     </>
-  )
+  );
 }
 
 //This a component, it can be used inside of various components
-// function Text({text}) { //{text} is called a prop, it a variable that be passed dinamically to a component 
+// function Text({text}) { //{text} is called a prop, it a variable that be passed dynamically to a component 
 //   return (
 //     <div>
 //       <p>{text}</p>
---
+++ frontend/src/pages/home.jsx
@@ -0,0 +1,36 @@
+import MovieCard from "../assets/components/MovieCard";
+import { useState } from "react";
+
+function Home() {
+    const [searchQuery,setSearchQuery] = useState()
+
+    const movies = [
+        {id:1, title: "John Wick", release_date:"2018"},
+        {id:2, title: "John Wick 2", release_date:"2020"},
+        {id:3, title: "John Wick 3", release_date:"2022"},
+        {id:4, title: "John Wick 4", release_date:"2024"},
+    ];
+    
+    const handleSearch = () => {
+
+    }
+
+    return(
+        <div className="home">
+            <form action="" onSubmit={handleSearch} className="search-form">
+                <input type="text" 
+                    placeholder= "Search For Movies... " 
+                    className="search-input"
+                />
+                <button type="submit" className="search-button">Search</button> 
+            </form>
+            <div className="movies-grid">
+                {movies.map((movie) => (
+                    <MovieCard movie = {movie} key = {movie.id}></MovieCard>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default Home //We do this export in order to use the file in other files in the project
