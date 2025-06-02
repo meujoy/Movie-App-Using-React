60716e17e8462694a252953f72459549d9bc1ace
meujoy <youssefayman742@gmail.com>
2025-06-02 21:09:17 +0300

    Implemented API use.

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,17 +1,13 @@
 import MovieCard from "../components/MovieCard";
 import { useState } from "react";
+import { getPopularMovies,searchMovies } from "../services/api";
 import '../css/Home.css'
 
 function Home() {
     //searchQuery is the current state value, setSearchQuery is the function to update the state
     const [searchQuery,setSearchQuery] = useState(""); 
 
-    const movies = [
-        {id:1, title: "John Wick", release_date:"2018"},
-        {id:2, title: "Terminator", release_date:"2020"},
-        {id:3, title: "Inception", release_date:"2022"},
-        {id:4, title: "John Wick 4", release_date:"2024"},
-    ];
+    
     
     const handleSearch = (e) => { // e is the event object that is passed automatically 
         e.preventDefault() // Prevents Default behaviour which refreshs the page after pressing the search button
